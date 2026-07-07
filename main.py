6599191ac8d44534756ccbac8a67b613aa15afc0
AdarshBobade <adarsh.bobade26@gmail.com>
2026-07-07 09:34:08 +0530

    Refactor main.py into providers.py and decorators.py

--- main.py
+++ main.py
@@ -5,73 +5,11 @@ import httpx
 from dotenv import load_dotenv
 load_dotenv()
 from abc import ABC,abstractmethod
+from providers import Groq , Gemini
 
 groq_api = os.getenv('GROQ_API_KEY')
 gemini_api = os.getenv('GEMINI_API_KEY')
 hf_api = os.getenv('HUGGINGFACE_TOKEN')
-
-class BaseProvider(ABC):
-    def __init__(self,api_key):
-        self.api_key = api_key
-        
-    @abstractmethod
-    async def fetch(self,prompt):
-        pass
-
-class Groq(BaseProvider):
-    def __init__(self,api_key):
-        super().__init__(api_key)
-        self.url = 'https://api.groq.com/openai/v1/chat/completions'
-    async def fetch(self,prompt):
-        async with httpx.AsyncClient(timeout=10) as client:
-            
-            headers = {'Authorization': f"Bearer {self.api_key}"}
-            body = {
-                    'model':'openai/gpt-oss-120b' , 
-                    'messages': [
-                        {
-                            'role':'user' , 
-                            'content':prompt
-                        }
-                        ]
-                    }
-            response = await client.post(self.url , headers=headers , json=body)
-            response.raise_for_status()
-
-        return response.json()['choices'][0]['message']['content']
-    
-
-class Gemini(BaseProvider):
-    def __init__(self,api_key):
-        super().__init__(api_key)
-        self.url = (
-                    "https://generativelanguage.googleapis.com/v1beta/"
-                    "models/gemini-2.5-flash:generateContent"
-                    )
-
-    async def fetch(self,prompt):
-        async with httpx.AsyncClient(timeout=10) as client:
-            headers = {
-                        "Content-Type": "application/json"
-                      }
-            params = {'key': self.api_key}
-            body = {
-                    'contents':[
-                        {
-                            'role':'user',
-                            'parts':[
-                                {
-                                'text':prompt
-                                }
-                                
-                                    ]
-                                }
-                            ]
-                    }
-            response = await client.post(self.url , headers=headers , params=params, json=body)
-            response.raise_for_status()
-
-        return response.json()['candidates'][0]['content']['parts'][0]['text']
 
 
 async def main():
---
+++ providers.py
@@ -0,0 +1,66 @@
+import httpx
+from abc import ABC , abstractmethod
+
+class BaseProvider(ABC):
+    def __init__(self,api_key):
+        self.api_key = api_key
+        
+    @abstractmethod
+    async def fetch(self,prompt):
+        pass
+
+class Groq(BaseProvider):
+    def __init__(self,api_key):
+        super().__init__(api_key)
+        self.url = 'https://api.groq.com/openai/v1/chat/completions'
+    async def fetch(self,prompt):
+        async with httpx.AsyncClient(timeout=10) as client:
+            
+            headers = {'Authorization': f"Bearer {self.api_key}"}
+            body = {
+                    'model':'openai/gpt-oss-120b' , 
+                    'messages': [
+                        {
+                            'role':'user' , 
+                            'content':prompt
+                        }
+                        ]
+                    }
+            response = await client.post(self.url , headers=headers , json=body)
+            response.raise_for_status()
+
+        return response.json()['choices'][0]['message']['content']
+    
+
+class Gemini(BaseProvider):
+    def __init__(self,api_key):
+        super().__init__(api_key)
+        self.url = (
+                    "https://generativelanguage.googleapis.com/v1beta/"
+                    "models/gemini-2.5-flash:generateContent"
+                    )
+
+    async def fetch(self,prompt):
+        async with httpx.AsyncClient(timeout=10) as client:
+            headers = {
+                        "Content-Type": "application/json"
+                      }
+            params = {'key': self.api_key}
+            body = {
+                    'contents':[
+                        {
+                            'role':'user',
+                            'parts':[
+                                {
+                                'text':prompt
+                                }
+                                
+                                    ]
+                                }
+                            ]
+                    }
+            response = await client.post(self.url , headers=headers , params=params, json=body)
+            response.raise_for_status()
+
+        return response.json()['candidates'][0]['content']['parts'][0]['text']
+
